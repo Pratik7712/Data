5ae408485fe652f87b5cc918ff8501404fb08190
Pratik <pratikahir787@gmail.com>
2023-12-22 18:19:49 +0530

    22/12

---
+++ Finance/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Hero from './assets/Components/Hero';
+import Navbar from './assets/Components/Navbar';
+
+function App() {
+ 
+  return (
+   <div>
+    <Navbar/>
+    <Hero/>
+   </div>
+  );
+}
+
+export default App
